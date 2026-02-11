f26c6c6d7a0d7abe3524a601d4ecc3603275a63d
Fardin Hasan <wizardfire77@gmail.com>
2026-02-12 01:55:56 +0600

    Rename class from lab_07_hw_1 to main

--- Array Binarization.java
+++ Array Binarization.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class lab_07_hw_1{
+public class main{
     public static void main(String[] args){
         Scanner sc=new Scanner(System.in);
         System.out.print("Enter the length of the array:");
